6d4708cbff7b6b794828e5659b0b50928a8b8883
Luiz Valverde <luizcastroval321@gmail.com>
2021-12-06 17:37:48 -0300

    13423-B

--- codeforces/lvl800/1343-B.cpp
+++ codeforces/lvl800/1343-B.cpp
@@ -8,8 +8,33 @@ void solve() {
 
   cin >> n;
 
-  if (!(n % 2 == 0 and (n % 2) % 2 == 0)) {
+  if ((n % 2 == 0) and ((n/2) % 2 == 0)) {
     cout << "YES" << endl;
+    int sol[n];
+    
+    int sumEven = 0;
+    int sumOdd = 0;
+
+    for(int i = 0; i<n/2; i++){
+      sol[i] = (i+1)*2;
+      sumEven += (i+1)*2;
+      
+      if (i < n/2 - 1)
+      {
+        sol[n/2 + i] = (i+1)*2 -1; //put odd numbers in order until the last one
+        sumOdd += (i+1)*2 - 1;
+      }
+      else
+      {
+        sol[n - 1] =  sumEven - sumOdd;
+      }
+    }
+
+    for(int i : sol){
+      cout << i << " ";
+    }
+    
+    cout << endl;
 
   } else {
     cout << "NO" << endl;
@@ -17,7 +42,19 @@ void solve() {
 }
 
 int main() {
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
   ios_base::sync_with_stdio(0);
 
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    solve();
+  }
+  
   return 0;
 }
